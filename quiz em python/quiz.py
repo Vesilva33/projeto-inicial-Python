27dc5b054e12030bede1d2a7eb7a75c0542ee0a2
Verusca silva <56118750+Vesilva33@users.noreply.github.com>
2023-03-11 20:29:51 -0300

    Quiz em python
    aprendendo a linguagem

---
+++ quiz em python/quiz.py
@@ -0,0 +1,44 @@
+print("Seja muito bem vindo ao quiz da verusca!")
+answer_user=input("Quer começar? [S/N]")
+
+if answer_user != "S":
+    quit()
+        
+
+print("começando....") 
+
+print ("Qual linguagem o jogo [Minecraft]? \n (A)JAVA \n (B)PYTHON \n (C)JAVASCRIPT \n (D) C++\n")     
+answer_1 = input("Resposta:  ")
+
+if answer_1 == "A":
+    print("correct!")
+    
+    
+    
+else:
+    print("incorreto")
+
+print ("Qual linguagem que domina a [WEB]? \n (A)JAVA \n (B)PYTHON \n (C)JAVASCRIPT \n (D) C++\n")     
+answer_1 = input("Resposta:  ")
+
+if answer_1 == "C":
+    print("correct!")
+    
+   
+    
+else:
+    print("incorreto")    
+    
+    
+print ("Qual linguagem programação e [NOVA]? \n (A)JAVA \n (B)PYTHON \n (C)JAVASCRIPT \n (D) C++\n")     
+answer_1 = input("Resposta:  ")
+
+if answer_1 == "B":
+    print("correct!")
+    
+    
+    
+else:
+    print("incorreto")    
+    
+print(f"Quiz acabou.")        
